77e5bdcce38be34aab0551775f7f8893c2c10d48
Lars Mudde <lars.mudde@capgemini.com>
2020-10-26 19:12:24 +0100

    Initial commit, spring initializr

---
+++ src/test/java/nl/showcase/memento/MementoApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.showcase.memento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MementoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
